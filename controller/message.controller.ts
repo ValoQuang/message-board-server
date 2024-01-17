b1778f5b187885c6bd521a19304b2a734b99d903
Q.ilot <45687913+ValoQuang@users.noreply.github.com>
2024-01-17 15:02:22 +0200

    fix: object POST

--- controller/message.controller.ts
+++ controller/message.controller.ts
@@ -48,11 +48,10 @@ export const createMessage = async (req: Request, res: Response) => {
   try {
     const channel = req.params.channel;
     const messageText = req.body;
-    channelStorage[channel].push({ id: Date.now(), text: messageText });
-
+    channelStorage[channel].push({ id: Date.now(), text: messageText.note });
     return res.status(201).json({
       code: res.statusCode,
-      message: "message registered successfully",
+      message: messageText
     });
   } catch (error) {
     res.status(500).json({
